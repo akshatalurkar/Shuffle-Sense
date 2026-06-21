dfd8b4623cd89f5b3a3965b33729b555270ee510
Akshat Alurkar <akshat@MacBookPro.lan>
2026-06-21 15:51:17 -0700

    Deployment url change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Projectofalltime_2/',
+  base: '/Shuffle-Sense/',
   plugins: [react(), tailwindcss()],
   test: {
     environment: 'jsdom',
