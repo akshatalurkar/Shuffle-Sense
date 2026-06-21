67e9fa5ffd0e9d314cd290d57a8945bc2f979606
Akshat Alurkar <akshat@Akshats-MacBook-Pro.local>
2026-06-20 18:48:40 -0700

    Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Projectofalltime_2/',
   plugins: [react(), tailwindcss()],
   test: {
     environment: 'jsdom',
